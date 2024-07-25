417d17325d2f42a454d5cacd69420e861b9428b1
yoshisuetakuya <t.yoshisue@cybergrow.co.jp>
2024-07-25 14:05:12 +0900

    first commit

---
+++ attendance-back/src/main/java/com/example/attendance_back/AttendanceBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.attendance_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceBackApplication.class, args);
+	}
+
+}
---
+++ attendance-back/src/test/java/com/example/attendance_back/AttendanceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.attendance_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
