2e51731e8e63a9fed2d281c5c43b91c9d06c9e2f
yoshisuetakuya <t.yoshisue@cybergrow.co.jp>
2024-08-09 17:14:23 +0900

    corsの設定の追加

--- attendance-back/src/main/java/com/example/attendance_back/controller/CreateEmployeeController.java
+++ attendance-back/src/main/java/com/example/attendance_back/controller/CreateEmployeeController.java
@@ -1,7 +1,7 @@
 package com.example.attendance_back.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +16,7 @@ import com.example.attendance_back.service.CreateUserService;
  *         ユーザーの新規登録を行うコントローラクラス
  *
  */
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class CreateEmployeeController {
 
