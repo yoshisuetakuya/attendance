a5f16a40cd0942f6571d6bd0d4ec6420d1620233
yoshisuetakuya <t.yoshisue@cybergrow.co.jp>
2024-08-20 16:57:19 +0900

    コード修正

--- attendance-back/src/main/java/com/example/attendance_back/controller/CreateEmployeeController.java
+++ attendance-back/src/main/java/com/example/attendance_back/controller/CreateEmployeeController.java
@@ -3,7 +3,6 @@ package com.example.attendance_back.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +17,6 @@ import com.example.attendance_back.service.CreateUserService;
  *         ユーザーの新規登録を行うコントローラクラス
  *
  */
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class CreateEmployeeController {
 
